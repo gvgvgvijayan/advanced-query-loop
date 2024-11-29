73e0790be25be3d06b3b9301bf69c4d148141d34
Ryan Welcher <me@ryanwelcher.com>
2024-11-29 11:24:03 -0500

    Commit before removing tabs in case I change my mind.

--- src/components/taxonomy-select.js
+++ src/components/taxonomy-select.js
@@ -1,3 +1,5 @@
+/* eslint-disable @wordpress/no-unsafe-wp-apis */
+
 /**
  * External dependencies
  */
@@ -6,7 +8,17 @@ import { v4 as uuidv4 } from 'uuid';
 /**
  * WordPress dependencies
  */
-import { Modal, Button, ButtonGroup } from '@wordpress/components';
+import {
+	ButtonGroup,
+	Button,
+	Dropdown,
+	__experimentalDropdownContentWrapper as DropdownContentWrapper,
+	TabPanel,
+	PanelBody,
+	ToggleControl,
+	Panel,
+	PanelRow,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useSelect } from '@wordpress/data';
 import { store as coreStore } from '@wordpress/core-data';
@@ -15,8 +27,8 @@ import { useState } from '@wordpress/element';
 /**
  * Internal dependencies
  */
-
 import SingleTaxonomyControl from './single-taxonomy-control';
+import SimpleTaxonomyControl from './simple-taxonomy-control';
 
 export const TaxonomySelect = ( { attributes, setAttributes } ) => {
 	const {
@@ -26,96 +38,277 @@ export const TaxonomySelect = ( { attributes, setAttributes } ) => {
 		} = {},
 	} = attributes;
 
-	const [ isOpen, setOpen ] = useState( false );
-	const openModal = () => setOpen( true );
-	const closeModal = () => setOpen( false );
-
 	const availableTaxonomies = useSelect( ( select ) =>
 		select( coreStore )
 			.getTaxonomies()
 			?.filter( ( { types } ) => types.includes( postType ) )
 	);
 
+	const [ toggled, setToggled ] = useState( false );
+
 	return (
 		<>
-			<Button variant="primary" onClick={ openModal } size="large">
-				{ __( 'Open Taxonomy query builder', 'advanced-query-loop' ) }
-			</Button>
-			<br />
-			{ isOpen && (
-				<Modal
-					title={ __(
-						'Taxonomy query builder',
-						'advanced-query-loop'
-					) }
-					onRequestClose={ closeModal }
-					size="large"
-				>
-					{ queries.map(
-						( {
-							id,
-							taxonomy,
-							terms,
-							include_children: includeChildren,
-							operator,
-						} ) => {
-							return (
-								<SingleTaxonomyControl
-									key={ id }
-									id={ id }
-									taxonomy={ taxonomy }
-									terms={ terms }
-									includeChildren={ includeChildren }
-									availableTaxonomies={ availableTaxonomies }
-									operator={ operator }
-									setAttributes={ setAttributes }
-									attributes={ attributes }
+			<Dropdown
+				popoverProps={ {
+					placement: 'left-start',
+					offset: 36,
+				} }
+				renderToggle={ ( { isOpen, onToggle } ) => (
+					<>
+						<Button
+							variant="primary"
+							onClick={ onToggle }
+							aria-haspopup="true"
+							aria-expanded={ isOpen }
+						>
+							{ isOpen
+								? __(
+										'Close Taxonomy query builder',
+										'advanced-query-loop'
+								  )
+								: __(
+										'Open Taxonomy query builder',
+										'advanced-query-loop'
+								  ) }
+						</Button>
+					</>
+				) }
+				renderContent={ () => (
+					<DropdownContentWrapper
+						paddingSize="none"
+						style={ { width: '30rem' } }
+					>
+						<Panel
+							header={ __(
+								'Taxonomy query builder',
+								'advanced-query-loop'
+							) }
+						>
+							<PanelBody>
+								{ queries.map(
+									( { id, taxonomy, terms, operator } ) => {
+										return (
+											<SimpleTaxonomyControl
+												key={ id }
+												taxonomy={ taxonomy }
+												operator={ operator }
+												terms={ terms }
+												id={ id }
+												availableTaxonomies={
+													availableTaxonomies
+												}
+												setAttributes={ setAttributes }
+												attributes={ attributes }
+											/>
+										);
+									}
+								) }
+
+								<ToggleControl
+									checked={ toggled }
+									label={ __(
+										'Enable advanced mode',
+										'advanced-query-loop'
+									) }
+									onChange={ () => setToggled( ! toggled ) }
+									__nextHasNoMarginBottom={ false }
 								/>
-							);
-						}
-					) }
-					<ButtonGroup>
-						<Button
-							variant="primary"
-							onClick={ () =>
-								setAttributes( {
-									query: {
-										...attributes.query,
-										tax_query: [],
-									},
-								} )
-							}
-						>
-							Reset
-						</Button>
-						<Button
-							variant="primary"
-							onClick={ () => {
-								setAttributes( {
-									query: {
-										...attributes.query,
-										tax_query: {
-											relation,
-											queries: [
-												...queries,
-												{
-													taxonomy: '',
-													terms: [],
-													include_children: true,
-													operator: 'IN',
-													id: uuidv4(),
-												},
-											],
-										},
-									},
-								} );
+							</PanelBody>
+						</Panel>
+
+						<TabPanel
+							className="taxonomy-query-builder"
+							activeClass="css-ms66sk"
+							tabs={ [
+								{
+									name: 'simple',
+									title: 'Simple Mode',
+								},
+							] }
+							children={ ( tab ) => {
+								switch ( tab.name ) {
+									case 'simple': {
+										return (
+											<PanelBody>
+												{ queries.map(
+													( {
+														id,
+														taxonomy,
+														terms,
+														operator,
+													} ) => {
+														return (
+															<SimpleTaxonomyControl
+																key={ id }
+																taxonomy={
+																	taxonomy
+																}
+																operator={
+																	operator
+																}
+																terms={ terms }
+																id={ id }
+																availableTaxonomies={
+																	availableTaxonomies
+																}
+																setAttributes={
+																	setAttributes
+																}
+																attributes={
+																	attributes
+																}
+															/>
+														);
+													}
+												) }
+
+												<ButtonGroup>
+													<Button
+														variant="primary"
+														onClick={ () => {
+															setAttributes( {
+																query: {
+																	...attributes.query,
+																	tax_query: {
+																		relation:
+																			relation ||
+																			'AND',
+																		queries:
+																			[
+																				...queries,
+
+																				{
+																					id: uuidv4(),
+																					taxonomy:
+																						'',
+																					terms: [],
+																					include_children: true,
+																					operator:
+																						'IN',
+																				},
+																			],
+																	},
+																},
+															} );
+														} }
+													>
+														Add new taxonomy query
+													</Button>
+
+													<Button
+														variant="primary"
+														onClick={ () =>
+															setAttributes( {
+																query: {
+																	...attributes.query,
+																	tax_query:
+																		[],
+																},
+															} )
+														}
+													>
+														Reset
+													</Button>
+												</ButtonGroup>
+											</PanelBody>
+										);
+									}
+									case 'advanced': {
+										return null;
+										return (
+											<PanelBody>
+												{ queries.map(
+													( {
+														id,
+														taxonomy,
+														terms,
+														include_children:
+															includeChildren,
+														operator,
+													} ) => {
+														return (
+															<SingleTaxonomyControl
+																key={ id }
+																id={ id }
+																taxonomy={
+																	taxonomy
+																}
+																terms={ terms }
+																includeChildren={
+																	includeChildren
+																}
+																availableTaxonomies={
+																	availableTaxonomies
+																}
+																operator={
+																	operator
+																}
+																setAttributes={
+																	setAttributes
+																}
+																attributes={
+																	attributes
+																}
+															/>
+														);
+													}
+												) }
+												<ButtonGroup>
+													<Button
+														size="small"
+														variant="primary"
+														isDestructive
+														onClick={ () =>
+															setAttributes( {
+																query: {
+																	...attributes.query,
+																	tax_query:
+																		[],
+																},
+															} )
+														}
+													>
+														Reset
+													</Button>
+													<Button
+														variant="primary"
+														size="small"
+														onClick={ () => {
+															setAttributes( {
+																query: {
+																	...attributes.query,
+																	tax_query: {
+																		relation,
+																		queries:
+																			[
+																				...queries,
+																				{
+																					taxonomy:
+																						'',
+																					terms: [],
+																					include_children: true,
+																					operator:
+																						'IN',
+																					id: uuidv4(),
+																				},
+																			],
+																	},
+																},
+															} );
+														} }
+													>
+														Add new taxonomy query
+													</Button>
+												</ButtonGroup>
+											</PanelBody>
+										);
+									}
+								}
 							} }
-						>
-							Add new taxonomy query
-						</Button>
-					</ButtonGroup>
-				</Modal>
-			) }
+						/>
+					</DropdownContentWrapper>
+				) }
+			/>
 		</>
 	);
 };
