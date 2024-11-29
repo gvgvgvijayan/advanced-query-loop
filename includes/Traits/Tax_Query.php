b58da73b0cbc26c14dba3761290bdce9c0bdfa53
Ryan Welcher <me@ryanwelcher.com>
2024-11-29 13:42:12 -0500

    Process booleans correctly and harden up the code. 💪

--- includes/Traits/Tax_Query.php
+++ includes/Traits/Tax_Query.php
@@ -13,21 +13,52 @@ trait Tax_Query {
 
 	public function parse_tax_query( $queries ) {
 		$tax_query = [];
-		// Handle the relation parameter.
-		if ( isset( $queries['relation'] ) && count( $queries['queries'] ) > 1 ) {
-			$tax_query['relation'] = $queries['relation'];
-		}
-
-		// Create the array for the queries
-		// Loop the queries
-		foreach ( $queries['queries'] as $query ) {
-			if ( isset( $query['taxonomy'] ) && isset( $query['terms'] ) && count( $query['terms'] ) > 0 ) {
-				$new_item          = array_filter( $query, fn( $key ) => 'id' !== $key, ARRAY_FILTER_USE_KEY );
-				$new_item['terms'] = [ ...array_map( fn( $term ) => get_term_by( 'name', $term, $query['taxonomy'] )->term_id, $query['terms'] ) ];
-				$tax_query[]       = $new_item;
+		// Don't process empty array of queries.
+		if ( count( $queries['queries'] ) > 0 ) {
+			// Handle the relation parameter.
+			if ( isset( $queries['relation'] ) && count( $queries['queries'] ) > 1 ) {
+				$tax_query['relation'] = $queries['relation'];
+			}
+			// Loop the queries
+			foreach ( $queries['queries'] as $query ) {
+				if ( isset( $query['taxonomy'] ) && isset( $query['terms'] ) && count( $query['terms'] ) > 0 ) {
+					$processed_query                     = array_filter( $query, fn( $key ) => 'id' !== $key, ARRAY_FILTER_USE_KEY );
+					$processed_query['include_children'] = filter_var( $query['include_children'], FILTER_VALIDATE_BOOLEAN );
+					$processed_query['terms']            = [ ...array_map( fn( $term ) => get_term_by( 'name', $term, $query['taxonomy'] )->term_id, $query['terms'] ) ];
+					$tax_query[]                         = $processed_query;
+				}
 			}
 		}
-		// Let's not return empty arrays.
-		return array_filter( $tax_query );
+		return $tax_query;
 	}
 }
+
+/**
+ * Example complex query:
+ * $tax_query = array(
+ *     'relation' => 'OR',
+ *     array(
+ *         'taxonomy' => 'category',
+ *         'field'    => 'slug',
+ *         'terms'    => array( 'quotes' ),
+ *     ),
+ *     array(
+ *         'taxonomy' => 'tag',
+ *         'field'    => 'slug',
+ *         'terms'    => array( 2 ),
+ *     ),
+ *     array(
+ *         'relation' => 'AND',
+ *         array(
+ *             'taxonomy' => 'post_format',
+ *             'field'    => 'slug',
+ *             'terms'    => array( 'post-format-quote' ),
+ *         ),
+ *         array(
+ *             'taxonomy' => 'category',
+ *             'field'    => 'slug',
+ *             'terms'    => array( 'wisdom' ),
+ *         ),
+ *     ),
+ * );
+ */
